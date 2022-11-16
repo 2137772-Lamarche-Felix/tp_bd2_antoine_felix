7afade29769f69914235d35720ac327d877c151d
Felix lamarche <2137772@etudiant.cegepvicto.ca>
2022-11-16 13:56:42 -0500

    Création de la procédure et du trigger

--- Scripts_SQL/06_functions.sql
+++ Scripts_SQL/06_functions.sql
@@ -1,7 +1,18 @@
 DELIMITER $$
-CREATE FUNCTION nom_function() RETURNS TYPE CARACTERISTIQUE_1 [CARACTERISTIQUE_2]
+CREATE procedure delete_sauvegarde() RETURNS TYPE (IN _id_sauvegarde INT)
 BEGIN
-	...
-	RETURN ;
+	DELETE FROM sauvegarde WHERE id=_id_sauvegarde; 
 END $$
+DELIMITER ;
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS nom_sauvegarde_trigger
+CREATE TRIGGER nom_sauvegarde_trigger BEFORE INSERT ON sauvegarde FOR EACH ROW 
+BEGIN
+DECLARE _count_sauvegarde INT;
+	SET _count_sauvegarde = (SELECT count(*) FROM sauvegarde WHERE nom = NEW.nom)
+	IF _count_sauvegarde > 0 THEN
+		SET NEW.nom = CONCAT(NEW.nom, "(", _count_sauvegarde")")
+	END IF ;
+END$$
 DELIMITER ;
